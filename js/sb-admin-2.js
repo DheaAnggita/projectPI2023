532389de88bcbb18387ef47212d6353713c89789
fidaatag <fidaa2fidaa@gmail.com>
2023-12-05 10:45:02 +0700

    add profile donation project

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -1,56 +1,65 @@
-(function($) {
-  "use strict"; // Start of use strict
+document.getElementById('sidebarToggleTop').addEventListener('click', function (e) {
+  document.body.classList.toggle("sidebar-toggled");
+  document.querySelector(".sidebar").classList.toggle("toggled");
+  if (document.querySelector(".sidebar").classList.contains("toggled")) {
+    document.querySelectorAll('.sidebar .collapse').forEach(function (collapse) {
+      collapse.collapse('hide');
+    });
+  }
+});
 
-  // Toggle the side navigation
-  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
-    $("body").toggleClass("sidebar-toggled");
-    $(".sidebar").toggleClass("toggled");
-    if ($(".sidebar").hasClass("toggled")) {
-      $('.sidebar .collapse').collapse('hide');
-    };
-  });
+// Close any open menu accordions when window is resized below 768px
+window.addEventListener('resize', function () {
+  if (window.innerWidth < 768) {
+    document.querySelectorAll('.sidebar .collapse').forEach(function (collapse) {
+      collapse.collapse('hide');
+    });
+  }
 
-  // Close any open menu accordions when window is resized below 768px
-  $(window).resize(function() {
-    if ($(window).width() < 768) {
-      $('.sidebar .collapse').collapse('hide');
-    };
-    
-    // Toggle the side navigation when window is resized below 480px
-    if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
-      $("body").addClass("sidebar-toggled");
-      $(".sidebar").addClass("toggled");
-      $('.sidebar .collapse').collapse('hide');
-    };
-  });
+  // Toggle the side navigation when window is resized below 480px
+  if (window.innerWidth < 480 && !document.querySelector(".sidebar").classList.contains("toggled")) {
+    document.body.classList.add("sidebar-toggled");
+    document.querySelector(".sidebar").classList.add("toggled");
+    document.querySelectorAll('.sidebar .collapse').forEach(function (collapse) {
+      collapse.collapse('hide');
+    });
+  }
+});
 
-  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
-    if ($(window).width() > 768) {
-      var e0 = e.originalEvent,
-        delta = e0.wheelDelta || -e0.detail;
+// Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+var sidebar = document.querySelector('body.fixed-nav .sidebar');
+if (sidebar) {
+  sidebar.addEventListener('wheel', function (e) {
+    if (window.innerWidth > 768) {
+      var delta = e.wheelDelta || -e.detail;
       this.scrollTop += (delta < 0 ? 1 : -1) * 30;
       e.preventDefault();
     }
   });
+}
 
-  // Scroll to top button appear
-  $(document).on('scroll', function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
+// Scroll to top button appear
+window.addEventListener('scroll', function () {
+  var scrollDistance = window.scrollY;
+  var scrollToTopButton = document.querySelector('.scroll-to-top');
+  if (scrollDistance > 100) {
+    scrollToTopButton.style.display = 'block';
+  } else {
+    scrollToTopButton.style.display = 'none';
+  }
+});
+
+// Smooth scrolling using vanilla JavaScript
+document.querySelectorAll('a.scroll-to-top').forEach(function (anchor) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    var target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      var offsetTop = target.getBoundingClientRect().top + window.scrollY;
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth'
+      });
     }
   });
-
-  // Smooth scrolling using jQuery easing
-  $(document).on('click', 'a.scroll-to-top', function(e) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: ($($anchor.attr('href')).offset().top)
-    }, 1000, 'easeInOutExpo');
-    e.preventDefault();
-  });
-
-})(jQuery); // End of use strict
+});
